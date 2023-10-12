eec94c340f5194ef7755228e7fa4a013851567a5
MatiasArno <amatiasarno@gmail.com>
2023-10-12 19:45:12 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { json } from 'express';
+
+const app = express();
+const PORT = process.env.PORT ?? 45000;
+
+app.use(json());
+
+app.get('/ping', (req, res) => {
+	res.json({
+		pong: 'Server running',
+		environment: process.env.ENVIRONMENT,
+	});
+});
+
+app.use('*', (req, res) => {
+	res.status(404).json({
+		error: 'Resource not found',
+	});
+});
+
+app.listen(PORT, () => {
+	console.log('Server listening on', PORT);
+});
